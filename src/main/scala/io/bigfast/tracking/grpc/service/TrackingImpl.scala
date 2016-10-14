0d07cecd31b76deb48d9c85f6d094141e3746e8e
Kenneth Lee <kenneth.lee@gmail.com>
2016-10-14 10:47:34 -0700

    use flow instead of straight source to sink

--- src/main/scala/io/bigfast/tracking/grpc/service/TrackingImpl.scala
+++ src/main/scala/io/bigfast/tracking/grpc/service/TrackingImpl.scala
@@ -1,25 +1,32 @@
 package io.bigfast.tracking.grpc.service
 
 import akka.actor.ActorSystem
-import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.Producer
-import akka.stream.scaladsl.Source
+import akka.kafka.{ProducerMessage, ProducerSettings}
+import akka.stream.scaladsl.{Sink, Source, SourceQueue}
 import akka.stream.{Materializer, OverflowStrategy}
 import io.bigfast.tracking.{Empty, Event, TrackingGrpc}
 import io.grpc.stub.StreamObserver
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
+import org.apache.kafka.common.serialization.ByteArraySerializer
 
 class TrackingImpl()(implicit val system:ActorSystem, val materilizer:Materializer) extends TrackingGrpc.Tracking {
   override def track(responseObserver: StreamObserver[Empty]): StreamObserver[Event]  = {
-    val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
+    val producerSettings = ProducerSettings(system, new ByteArraySerializer, new ByteArraySerializer)
 
-    val queue = Source.queue[Event](100000, OverflowStrategy.backpressure)
-      .map { event =>
-        new ProducerRecord[Array[Byte], String]("event", event.toString)
-      }
-      .to(Producer.plainSink(producerSettings))
-      .run()
+    var queue:SourceQueue[Event] = null
+    Source.queue[Event](100000, OverflowStrategy.backpressure).mapMaterializedValue { q => queue = q }
+    .map { event =>
+      ProducerMessage.Message(new ProducerRecord[Array[Byte], Array[Byte]]("event", event.toByteArray), event.id)
+    }
+    .via(Producer.flow(producerSettings)).map { result =>
+      val record = result.message.record
+      val event = Event.parseFrom(record.value)
+      println(s"${record.topic}/${record.partition} ${result.offset}: ${event}" +
+        s"(${result.message.passThrough})")
+      result
+    }
+    .runWith(Sink.ignore)
 
     new StreamObserver[Event] {
       override def onError(t: Throwable): Unit = {
